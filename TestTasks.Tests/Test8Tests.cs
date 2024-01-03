4ea476d0fa5663329cb66bd75d6da1863cd34dc6
Daniil <dadyarri@gmail.com>
2024-01-04 00:52:38 +0300

    add test8 implementation and some test for it

---
+++ TestTasks.Tests/Test8Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Threading;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestTasks.Tests
+{
+    public class Test8Tests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public Test8Tests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void GetCurrentUser()
+        {
+            var ti = new TestImplementation();
+            for (var i = 0; i < 20; i++)
+            {
+                var i1 = i;
+                var thread = new Thread(() =>
+                {
+                    ti.PrepareEnvironment(i1.ToString(), CultureInfo.CurrentCulture);
+                    _testOutputHelper.WriteLine(i1.ToString());
+                    ti.GetCurrentUser().Should().Be(i1.ToString());
+                });
+                thread.Name = $"Test8 thread #{i1}";
+                thread.Start();
+            }
+        }
+    }
+}
---
+++ TestTasks/TestImplementation.Test8.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Threading;
+using TestTasks.Abstract;
+
+namespace TestTasks
+{
+    public partial class TestImplementation : ITest8
+    {
+        private readonly ThreadLocal<UserInfo> _threadState = new ThreadLocal<UserInfo>();
+
+        public void PrepareEnvironment(string userName, CultureInfo userCulture)
+        {
+            _threadState.Value = new UserInfo
+            {
+                Username = userName,
+                CultureInfo = userCulture
+            };
+        }
+
+        public string GetCurrentUser()
+        {
+            return _threadState.Value.Username;
+        }
+    }
+
+    public class UserInfo
+    {
+        public string Username { get; set; }
+        public CultureInfo CultureInfo { get; set; }
+    }
+}
