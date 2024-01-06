ad4f045e97e4160fcd724bb5482be774751fff14
Daniil <dadyarri@gmail.com>
2024-01-06 20:41:32 +0300

    add test 7 implementation

---
+++ TestTasks/TestImplementation.Test7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTasks.Abstract;
+
+namespace TestTasks
+{
+    public partial class TestImplementation : ITest7
+    {
+        /// <summary>
+        /// Синхронно вычислить сразу все хэши, задействовав при этом все возможные ресурсы CPU, 
+        /// чтобы вернуть результат как можно скорее
+        /// </summary>
+        /// <param name="sourceArrays"></param>
+        /// <returns></returns>
+        public int[] CalculateHashes(byte[][] sourceArrays)
+        {
+            var hashes = new int[sourceArrays.Length];
+
+            Parallel.ForEach(sourceArrays.AsParallel().WithDegreeOfParallelism(Environment.ProcessorCount),
+                sourceData => hashes[Array.IndexOf(sourceArrays, sourceData)] =
+                    AssignedCalculator.GetVeryHardCalculatedHash(sourceData));
+
+            return hashes;
+        }
+    }
+}
